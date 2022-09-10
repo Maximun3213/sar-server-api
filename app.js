353ffa1435b6edfb8f47c882365f3b4eec07094e
Đạt  Huỳnh <datpanda3@gmail.com>
2022-09-10 20:01:22 +0700

    upd lib

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const multer = require("multer")
 const app = express();
+var cors = require('cors')
 app.use(express.static('uploads'))
 
 app.use(express.json());
+app.use(cors());
 
+var corsOptions = {
+  origin: 'https://google.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");

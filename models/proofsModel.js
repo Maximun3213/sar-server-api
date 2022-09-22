bb1c12749f93529fab467600e3c60bbd3be74f27
Đạt  Huỳnh <datpanda3@gmail.com>
2022-09-22 16:29:35 +0700

    upd

--- models/proofsModel.js
+++ models/proofsModel.js
@@ -38,10 +38,8 @@ const proofFileSchema = new Schema({
     type: Number,
     required: false,
   },
-  
-});
 
-// const Proof = mongoose.model("proof", ProofSchema);
+});
 
 const proofFolder = mongoose.model("proof_folder", proofFolderSchema);
 

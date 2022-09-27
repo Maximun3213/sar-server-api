ce8c618a9fdcef7910f7633ef62ed527d1fe1929
doboianh <anhb1809674@student.ctu.edu.vn>
2022-09-27 16:45:51 +0700

    upd-270922

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { userLogin, userList, userRegister, getAllProofManager, grantProofKey, getOwnStorage } = require("../controller/UserController");
+const { userLogin, userList, userRegister, getAllProofManager, grantProofKey } = require("../controller/UserController");
 const { authenToken } = require('../middleware/verifyToken');
 
 const router = express.Router();
@@ -15,6 +15,6 @@ router.route("/getAllProofManager").get(getAllProofManager)
 
 router.route("/grantProofKey").put(grantProofKey)
 
-router.route("/getOwnStorage/:id").get(getOwnStorage)
+// router.route("/getOwnStorage/:id").get(getOwnStorage)
 
 module.exports = router;

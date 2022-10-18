c42bc9a6da0b4fcdffb5fda277c878b023074e82
Đạt  Huỳnh <datpanda3@gmail.com>
2022-10-18 09:07:38 +0700

    upd socket

--- services/notify.js
+++ services/notify.js
@@ -1,15 +1,37 @@
+let onlineUsers = [];
+
+const addNewUser = (idUser, socketId) => {
+  !onlineUsers.some((user) => user.idUser === idUser) &&
+    onlineUsers.push({ idUser, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (idUser) => {
+  return onlineUsers.find((user) => user.idUser === idUser);
+};
+
 class SocketServices {
 
-  connection(socket){
-    socket.on('disconnect',()=>{
-      console.log(`User disconnect id is ${socket.id}`)
-    })
+  connection(socket) {
+    socket.on("disconnect", () => {
+      console.log(`User disconnect id is ${socket.id}`);
+      removeUser(socket.id);
+    });
 
     //on
-    socket.on('send_notify', (mess)=>{
-      console.log(mess)
-      _io.emit('receive_notify', mess)
-    })
+    socket.on("newUser", (idUser) => {
+      addNewUser(idUser, socket.id);
+      console.log(idUser)
+    });
+
+    socket.on("send_notify", ({ receiverID }) => {
+      const receiver = getUser(receiverID);
+      console.log(receiver.socketId)
+      _io.to(receiver.socketId).emit("receive_notify", receiverID);
+    });
   }
 }
 
